dda072f831ac980185ff1914344649f11303480a
unknown <069javi690@gmail.com>
2021-01-02 20:48:37 +0100

    unica ruta read

--- src/main/java/com/example/Reto/controllers/RutaController.java
+++ src/main/java/com/example/Reto/controllers/RutaController.java
@@ -1,11 +1,13 @@
 package com.example.Reto.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +28,9 @@ public class RutaController {
 		public List<Rutas> getRutas(){
 			return repository.findAll();
 		}
-
+		
+		@GetMapping("/{id}")
+		public Optional<Rutas> getRuta(@PathVariable String id){
+			return repository.findById(id);
+		}
 }
--- src/main/java/com/example/Reto/repositories/RutasRepository.java
+++ src/main/java/com/example/Reto/repositories/RutasRepository.java
@@ -1,9 +1,12 @@
 package com.example.Reto.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.Reto.models.Rutas;
 
 public interface RutasRepository extends MongoRepository<Rutas, String> {
-
+	Optional<Rutas> findById(String id);
 }
