78927b53057ecf9b5d970fe34b62dcbd1833f696
2dam3 <2dam3@hz114479.H012108.NET>
2021-01-11 12:55:36 +0100

    login funcional

--- src/main/java/com/example/Reto/controllers/UsuarioController.java
+++ src/main/java/com/example/Reto/controllers/UsuarioController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Reto.repositories.UsuarioRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.example.Reto.models.Usuario;
 @RequestMapping("/usuarios")
 @RestController
@@ -36,9 +40,24 @@ public class UsuarioController {
 //	    	System.out.println(user.getPassword());
 //	    	System.out.println(usuario.getPassword());
 //	    	System.out.println(user.getId());
+	    	String token = "";
+	    	try {
+	    	    Algorithm algorithm = Algorithm.HMAC256("secret");
+	    	    token = JWT.create()
+	    	        .withIssuer("auth0")
+	    	        .sign(algorithm);
+	    	    JWTVerifier verifier = JWT.require(algorithm)
+	    	    	    .acceptLeeway(454564)   //1 sec for nbf and iat
+	    	    	    .acceptExpiresAt(43200)
+	    	    	    .build();
+	    	} catch (JWTCreationException exception){
+	    	    //Invalid Signing configuration / Couldn't convert Claims.
+	    	}
+
 	    	JSONObject json = new JSONObject();
-	    	json.put("usuario", user.getUsuario());
-	    	
+	    	json.put("user", user.getUsuario());
+	    	json.put("token", token);
+
 	    	if(usuario.getPassword().equals(user.getPassword())){//El error 500 que da es en esta línea, a ver si veis porque e
 	    		json.put("status", 200);
 	    		return json;
