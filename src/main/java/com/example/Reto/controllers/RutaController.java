414f968be43f3854ae14f527ee62c36981b0010d
2dam3 <2dam3@hz114479.H012108.NET>
2021-01-12 12:54:01 +0100

    guardado de rutas y comienzo de preguntas

--- src/main/java/com/example/Reto/controllers/RutaController.java
+++ src/main/java/com/example/Reto/controllers/RutaController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,9 @@ public class RutaController {
 		@GetMapping("/{id}")
 		public Optional<Rutas> getRuta(@PathVariable String id){
 			return repository.findById(id);
+		}
+		@PostMapping("/save")
+		public Rutas saveRuta(@RequestBody Rutas ruta){
+			return repository.save(ruta);
 		}
 }
